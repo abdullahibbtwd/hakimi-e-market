013e1022fc12f6243368b4e7cebe9548a8935289
abdullahibbtwd <abdullahibbtwd@gmail.com>
2025-05-13 02:11:14 -0700

    deletesome

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,9 +1,17 @@
 "use server"
 import { signIn,signOut } from "@/auth";
+type ProviderId = string;
 
-export async function doSocialLogin(formData) {
+export async function doSocialLogin(formData: FormData): Promise<void> {
     const action = formData.get('action');
-    await signIn(action,{redirectTo:"/home"})
+//     await signIn(action,{redirectTo:"/home"})
+        
+    if (action) {
+        await signIn(action as ProviderId, { redirectTo: "/home" });
+    } else {
+       
+        console.error("Action is missing");
+    }
 }
 export async function doLogout() {
     await signOut({redirectTo:"/"}) 
